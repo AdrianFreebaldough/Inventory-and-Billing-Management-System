18aaf669093de5d05e0dec5267e801a3658cbe76
AdrianFreebaldough <adrianfrivaldo06@gmail.com>
2026-02-20 21:45:04 +0800

    add routes to inventory

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -1,5 +1,27 @@
-// Inventory routes
-const express = require('express');
+import express from "express";
+import { protect } from "../middleware/AuthMiddlewareUser.js";
+import { authorizeRoles } from "../middleware/roleMiddleware.js";
+
 const router = express.Router();
 
-module.exports = router;
+/* OWNER ONLY */
+router.post(
+  "/add",
+  protect,
+  authorizeRoles("owner"),
+  (req, res) => {
+    res.json({ message: "Inventory item added" });
+  }
+);
+
+/* OWNER + STAFF */
+router.get(
+  "/",
+  protect,
+  authorizeRoles("owner", "staff"),
+  (req, res) => {
+    res.json({ message: "Inventory list" });
+  }
+);
+
+export default router;
